9f3ddba562586b0cbb767067642cf16a1f26ee57
McGiver- <gkolampas@gmail.com>
2015-06-04 17:41:48 -0400

    added Better Query which Extend the Query class and offers static methods to be able to query as one would in prolog

--- src/PrologFun.java
+++ src/PrologFun.java
@@ -9,23 +9,39 @@ public class PrologFun {
 
 		Query q1 = new Query("consult(\'c:/users/george/workspace/prolog/Midterm.pl/')");
 		System.out.println("consult " + (q1.hasSolution() ? "succeeded" : "failed"));
-	/*    Variable X = new Variable();
-	    Query q4 =
-	        new Query(
-	            "edge",
-	            new Term[] {new Atom("a"),X}
-	        );
-	*/
-		Query q2 =new Query("factorial(3,X)");
-		System.out.println(q2.oneSolution().get("X"));
-		Query q3 = new Query("numbersBetween(1,10,R");
-		Hashtable[] R = q3.allSolutions().get("R");
-		System.out.println(R);
-//		Variable X = new Variable();
-//		Query q2 = new Query("factorial",new Term[] {new Atom("1234"),X } );
-//		java.util.Hashtable solution;
-//		solution = q2.getSolution();
-//		System.out.println("factorial of 1234 is"+solution.get(X));
+		
+		BetterQuery.ask("gcd(9,3,G)","G");
+		
+		BetterQuery.ask("rev([1,2,3,4],F)","F");
+		
+		BetterQuery.ask("assert(man(mario))");
+		
+		BetterQuery.ask("man(mario)");
+	}
+	public static class BetterQuery extends Query{
+
+		public BetterQuery(String arg0) {
+			super(arg0);
+		}
+		public static void ask(String toAsk, String... args){
+			Query q4 = new Query(toAsk);
+			Term listTerm1;
+			for(int i = 0;i<args.length;i++){
+				listTerm1= (Term)q4.oneSolution().get(args[i]);
+				System.out.println();
+				try{
+					for (Term term1 : listTerm1.toTermArray()) {
+						System.out.print(term1+" ");
+					}	
+					System.out.println();
+				}catch(JPLException p) {
+					System.out.println(listTerm1.toString());
+				}
+			}	
+		}
+		public static void ask(String toAsk){
+			System.out.println(Query.hasSolution(toAsk));
+		}
 	}
 }
 
